ef4cbf565e7bef9a998afce5aff6deb9f108c1a1
vipulsinha97 <60478456+vipulsinha97@users.noreply.github.com>
2024-10-20 00:59:51 +0530

    Add files via upload

--- app/Http/Controllers/signupController.php
+++ app/Http/Controllers/signupController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreRegistrationRequest;
-use App\Services\RegistrationServices;
+use App\Http\Requests\StoreRegistrationRequest; 
+use App\Services\RegistrationServices; 
 
 class SignupController extends Controller
 {
     protected $registrationServices;
 
-    public function __construct(RegistrationServices $registrationServices)
+    public function __construct(RegistrationServices $registrationServices) 
     {
         $this->registrationServices = $registrationServices;
     }
