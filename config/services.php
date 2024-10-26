463df149cb85ef742d9afe5b385992d810754963
Vipul Sinha <vipulsinha16.vs@gmail.com>
2024-10-27 01:31:56 +0530

    Vipul_Sinha: Facebook login

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => 'https://notjustswipe.com//facebook/callback',
+    ],
+
 ];
