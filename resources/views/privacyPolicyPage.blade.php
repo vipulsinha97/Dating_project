41c8abeda2cdfb3d71e34dd042c30a1fcae069cc
Amir Hasan <hasan.amir8@gmail.com>
2025-01-03 15:01:12 +0530

    Amir_Hasan:Privacy page designed

--- resources/views/privacyPolicyPage.blade.php
+++ resources/views/privacyPolicyPage.blade.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Privacy Policy</title>
-    </head>
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privacy Policy</title>
+    @include("includes/top")
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <style>
+        p {
+            text-align: left;
+    font-family: Roboto;
+    font-size: 17px;
+    font-weight: 500;
+    line-height: 29px;
+    color: #000;
+    text-decoration: none;
+        }
+    </style>
+</head>
+
+<body>
+    @include("includes/header")
+    <main>
+        <section class="container-fluid my-5">
         <h1>Privacy Policy</h1>
 
         <h2>Data we collect and usage</h2>
@@ -27,10 +44,10 @@
         <p>As part of photo verification, we will scan each photo that you submit. The scan may include the use of facial recognition technology so that we can compare the photo you submit to your profile photo, to help ensure that you are who you say you are. We do not add the selfie verification photos to your profile. We retain the scans so that we can verify you in the future and for our record-keeping purposes until you delete your profile. </p>
 
         <h2>The information the User provides at the Site, may come to NotJustSwipe through the following sources:</h2>
-        <p>Log  Files - Every time the User visits the Site, NotJustSwipe servers automatically gather information from User browser (such as IP addresses, browser type, Internet service provider (ISP), referring/exit pages, platform type, date/time stamp, and number of clicks) to analyze trends, administer the site, prevent fraud, track visitor movement in the aggregate, and gather broad demographic information. This information provides an idea of which parts of Site the User visits. Log files are not shared externally.</p>
+        <p>Log Files - Every time the User visits the Site, NotJustSwipe servers automatically gather information from User browser (such as IP addresses, browser type, Internet service provider (ISP), referring/exit pages, platform type, date/time stamp, and number of clicks) to analyze trends, administer the site, prevent fraud, track visitor movement in the aggregate, and gather broad demographic information. This information provides an idea of which parts of Site the User visits. Log files are not shared externally.</p>
 
         <p>Cookies - "Cookies" are used to keep track of some types of information. These are very small files placed on User"s computer, which allow NotJustSwipe to count the number of visitors to the Site and distinguish repeat visitors from new visitors. Cookies allow NotJustSwipe to save user preferences and track user trends. Please bear in mind if the user browser is set to reject all cookies, the Users experience may not be optimum and the Site may not function properly. Cookies are not linked to any Personally Identifiable Information and Users who refuse cookies assume all responsibility for any resulting loss of functionality.
-        Web Beacons. "Web beacons" are small transparent graphic images that are often used in conjunction with cookies in order to further personalize the Site for Users and to collect a limited set of information about the visitors to the Site. Web beacons could be used in email communications in order to understand the behavior of customers. Web beacons are not linked to any Personally Identifiable Information.</p>
+            Web Beacons. "Web beacons" are small transparent graphic images that are often used in conjunction with cookies in order to further personalize the Site for Users and to collect a limited set of information about the visitors to the Site. Web beacons could be used in email communications in order to understand the behavior of customers. Web beacons are not linked to any Personally Identifiable Information.</p>
 
         <p>Mobile Analytics. Mobile analytics software could be used to allow NotJustSwipe to better understand the functionality of our mobile software on User phone. This software may record information such as how often the User uses the application, the events that occur within the application, aggregated usage, performance data, and where the application was downloaded from. This information is not linked to any Personally Identifiable Information.</p>
 
@@ -38,12 +55,12 @@
 
         <p>Payment /Purchase Information. To process payment/ purchases, NotJustSwipe may require User name, address, phone number, email address and credit card information. Such information is used primarily to process User order or as otherwise described herein. NotJustSwipe adheres to strict security guidelines with respect to the storage, possession and handling of such information and protecting it against unauthorized access. It is clarified that NotJustSwipe does not store User’s credit card information. User transaction on NotJustSwipe is authorized and being processed by PayU/ HDFC payment gateway/ any other authorized payment gateway directly without any information passing through NotJustSwipe. Thus, all information related to credit/ debit card is stored/ held with our authorized payment gateway.. NotJustSwipe, handles the information only in accordance with the provisions of the applicable law as made applicable from time to time and only for the duration lawfully required, thereunder.</p>
 
-        <p>E-mails and Telephone Calls. User e-mail address is required while registering for the Services.          
+        <p>E-mails and Telephone Calls. User e-mail address is required while registering for the Services.
             E-mails are used for both transactional (e.g., confirmation for registration with the Site, credits, notifying of matches) and promotional (e.g., newsletters, new services, special discounts, etc.) purposes.
-        Mobile Device. If User has registered any mobile device or uses the mobile device to access the Site, then device information may get collected (such as mobile device ID, model and manufacturer), operating system and version information, and IP address .</p>
+            Mobile Device. If User has registered any mobile device or uses the mobile device to access the Site, then device information may get collected (such as mobile device ID, model and manufacturer), operating system and version information, and IP address .</p>
 
         <p>Demographic Data. Demographic data could be collected on the Site. This data is used to tailor User’s experience at the Site, showing content that which might interest them, and displaying the content according to their preferences. Some of this information may be shared with advertisers on a non-personally identifiable basis.
-        Online Survey Data. Information may be shared periodically during any voluntary member surveys. Users are encouraged to participate in such surveys as it helps in improving the Services. Survey responses are not linked to personally identifiable information, and all responses are anonymous.</p>
+            Online Survey Data. Information may be shared periodically during any voluntary member surveys. Users are encouraged to participate in such surveys as it helps in improving the Services. Survey responses are not linked to personally identifiable information, and all responses are anonymous.</p>
 
         <p>Information Regarding User Friends. The Site encourages Users to refer a friend for the Services by sending us a friend’s name and email address. This information remains in our database, and such referred person could be sent a one-time e-mail containing User’s name and inviting them to visit the Site. Such e-mail will also include instructions on how to remove their information from NotJustSwipe database. User agrees not to abuse this feature by entering names and addresses of those who would not be interested in the Services.</p>
 
@@ -57,11 +74,11 @@
         <p>Disclosure to User’s Matches. For providing the Services NotJustSwipe will disclose User profile information and verification results to recommended/ suggested matches. Photos and other profile information posted by User will be available for viewing. NotJustSwipe could make features available enabling User matches to share User profile information, including photos, with members of their social network(s). User contact information is never shared with User matches.</p>
 
         <p>User agrees and confirms that non-abidance of Verification requests made by NotJustSwipe will render the Registered Account at the Site terminable at the instance of NotJustSwipe without any recourse, whatsoever. NotJustSwipe is authorised and justified in taking any action necessary on such refusal by the User.
-        User agrees and understands that NotJustSwipe or its third party expert service provider and their associates are not the author or creator of the information coming forth in the Verification Checks. Under this premise and limitations, NotJustSwipe or its third party expert service provider or their associates do not warrant accuracy, validity or completeness of the content of the Verification checks. NotJustSwipe, expressly disclaims that the Verification checks will meet the User’s aspirations or expectations and shall be performed in an uninterrupted manner. Verification check verifies the existence of the facts and information provided by the User, it does not verify the authenticity of the facts itself.</p>
+            User agrees and understands that NotJustSwipe or its third party expert service provider and their associates are not the author or creator of the information coming forth in the Verification Checks. Under this premise and limitations, NotJustSwipe or its third party expert service provider or their associates do not warrant accuracy, validity or completeness of the content of the Verification checks. NotJustSwipe, expressly disclaims that the Verification checks will meet the User’s aspirations or expectations and shall be performed in an uninterrupted manner. Verification check verifies the existence of the facts and information provided by the User, it does not verify the authenticity of the facts itself.</p>
 
         <p>Disclosure By Law. User acknowledges and agrees that NotJustSwipe may disclose information User provides if required to do so by law, at the request of a third party, or if in NotJustSwipe’s sole discretion, believe that disclosure is reasonable to comply with the law, requests or orders from law enforcement, or any legal process (whether or not such disclosure is required by applicable law); protect or defend NotJustSwipe’s, or a third party’s, rights or property; protect someone’s health or safety, such as when harm or violence against any person is threatened; or for investigative purposes.
-        Search Engine. Some portions of User profile may be available to search engine crawlers and therefore may appear in internet search results, but no Personally Identifiable Information will be included in such search results.
-        Information Transferred As a Result of Sale of Business. NotJustSwipe may buy or sell assets and, depending on the transaction, User Personally Identifiable Information may be one of the transferred and acquired assets.</p>
+            Search Engine. Some portions of User profile may be available to search engine crawlers and therefore may appear in internet search results, but no Personally Identifiable Information will be included in such search results.
+            Information Transferred As a Result of Sale of Business. NotJustSwipe may buy or sell assets and, depending on the transaction, User Personally Identifiable Information may be one of the transferred and acquired assets.</p>
 
 
         <h2>Links to or Access from Other Sites</h2>
@@ -69,15 +86,21 @@
 
         <h2>Information Sharing</h2>
         <p>NotJustSwipe does not rent, sell or share any personal or sensitive information of User other than as mentioned in this Privacy Policy, with any third party.
-        Opt-out Provisions
+            Opt-out Provisions
 
-        User has the right to opt-in or opt-out of any marketing/promotional/newsletter mailings. Upon opting out, provisions of this Terms and Conditions and the related policies will become applicable. However, NotJustSwipe reserves the right to send service related communication without offering User the facility to opt-out. User can update your account settings and information at any time. At User"s request, NotJustSwipe can cancel User registration and remove/block personally identifiable information from it"s database. However, this information may remain in archives even after deletion or termination of User account as required by law and the handling of the same is in accordance with the provisions of the applicable law and as intimated by the government agency from time to time.
+            User has the right to opt-in or opt-out of any marketing/promotional/newsletter mailings. Upon opting out, provisions of this Terms and Conditions and the related policies will become applicable. However, NotJustSwipe reserves the right to send service related communication without offering User the facility to opt-out. User can update your account settings and information at any time. At User"s request, NotJustSwipe can cancel User registration and remove/block personally identifiable information from it"s database. However, this information may remain in archives even after deletion or termination of User account as required by law and the handling of the same is in accordance with the provisions of the applicable law and as intimated by the government agency from time to time.
 
-        You can contact our support team at contact@NotJustSwipe.com through your registered email address with a request to delete your data from our records.</p>
+            You can contact our support team at contact@NotJustSwipe.com through your registered email address with a request to delete your data from our records.</p>
 
         <h2>Security Practices</h2>
         <p>NotJustSwipe follows stringent security techniques and requirements for handling sensitive and personal information. These techniques and requirements are fully compliant with the guidelines set forth under law. NotJustSwipe servers are accessible only to authorized personnel. User information logged under the Terms and Conditions is shared with respective personnel only on need to know basis and/ or for the purpose of completing a transaction and/ or to providing Services. All representatives handling information under these provisions are under contractual confidentiality obligation with NotJustSwipe. While, NotJustSwipe takes every step to safeguard the confidentiality of User personal information, by using the Site, User agrees and confirms that NotJustSwipe is not liable for any disclosure due to errors in transmission or unauthorized acts of third parties.</p>
 
+        </section>
+    </main>
+    @include("includes/footer")
+    @include("includes/bottom")
 
-    </body>
+    <script src="assets/js/homepage-script.js"></script>
+</body>
+
 </html>
